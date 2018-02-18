44935898e93c76dd18da34f32985a90b0d7eb057
Michael Miao <maydaymiao@126.com>
2018-02-18 22:04:33 +0800

    Update Publisher.py

--- MQTT/Publisher.py
+++ MQTT/Publisher.py
@@ -1,34 +1,37 @@
+# -*- coding: utf-8 -*-
 # Import package
 import paho.mqtt.client as mqtt
 
-# Define Variables
-MQTT_BROKER = "broker ip address"
-MQTT_PORT = 1883
-MQTT_KEEPALIVE_INTERVAL = 45
-MQTT_TOPIC = "testTopic"
-MQTT_MSG = "Hello MQTT"
+broker = 'your ip address'
+broker_username = 'admin'
+broker_password = 'admin'
+topic = 'test'
+
+# Define event callbacks
+def on_connect(client, userdata, flags, rc):
+    print("rc: " + str(rc))
+
+def on_message(client, obj, msg):
+    print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+
+def on_publish(client, obj, mid):
+    print("mid: " + str(mid))
+
+def on_subscribe(client, obj, mid, granted_qos):
+    print("Subscribed: " + str(mid) + " " + str(granted_qos))
+
+def on_log(client, obj, level, string):
+    print(string)
 
 
-# Define on_connect event Handler
-def on_connect(mosq, obj, rc):
-	print "Connected to MQTT Broker"
-
-# Define on_publish event Handler
-def on_publish(client, userdata, mid):
-	print "Message Published..."
-
-# Initiate MQTT Client
 mqttc = mqtt.Client()
 
-# Register Event Handlers
-mqttc.on_publish = on_publish
+# Assign event callbacks
+mqttc.on_message = on_message
 mqttc.on_connect = on_connect
+mqttc.on_publish = on_publish
+mqttc.on_subscribe = on_subscribe
 
-# Connect with MQTT Broker
-mqttc.connect(MQTT_BROKER, MQTT_PORT, MQTT_KEEPALIVE_INTERVAL) 
+mqttc.username_pw_set(broker_username, broker_password)
+mqttc.connect(broker,1883)
 
-# Publish message to MQTT Topic 
-mqttc.publish(MQTT_TOPIC,MQTT_MSG)
-
-# Disconnect from MQTT_Broker
-mqttc.disconnect()
